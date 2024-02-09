8da9d076c8f7785e5161da1a289f8c32d0d29df7
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-02-08 18:51:15 -0700

    models done

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const isEmail = function(email) {
+    let regex = /^.+@(?:[\w-]+\.)+\w+$/
+    return regex.test(email)
+}
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, 
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: { type: String,
+        required: true,
+        unique: true,
+        validate: [ isEmail, 'Invalid Email' ]
+    },
+    friends: Array,
+    thoughts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+})
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length    
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
