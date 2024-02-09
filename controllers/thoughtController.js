35bb36439161ca8dbc06756dd1af25f72e1204e7
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-02-08 19:38:46 -0700

    editing controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,2 +1,45 @@
 const { User, Thought } = require('../models')
 const mongoose = require('mongoose')
+
+module.exports = {
+    async getThought(req, res){
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId }).select('-__v');
+            if (!thought) return res.status(404).json({ message: 'No thought found' })
+            res.json(thought)
+        } catch (err) {
+            console.error({ message: err })
+            return res.status(500).json(err)
+        }
+    },
+
+    async getThoughts(req, res){
+        try {
+            const thoughts = await Thought.find()
+            res.json(thoughts)
+        } catch (err) {
+            console.error({ message: err })
+            return res.status(500).json(err)
+        }
+    },
+
+    async createThought(req, res){
+        try {
+            const user = await User.findOne({ username: req.body.username }).select('__v')
+            if(!user) return res.status(404).json({ message: 'No user found' })
+
+            const thought = await Thought.create(req.body)
+            if(!thought) return res.status(500).json({ message: 'Creation failure' })
+
+            await User.findOneAndUpdate(
+                { username: req.body.username },
+                { $push: {thoughts: thought._id.toString()}}
+            )      
+            res.json(thought)
+
+        } catch (err) {
+            console.error({ message: err })
+            return res.status(500).json(err)
+        }
+    },
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,32 @@
 const { User, Thought } = require('../models')
 const mongoose = require('mongoose')
+
+module.exports = {
+    async getUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId }).populate('thoughts')
+
+            if(!user) return res.status(404).json({ message: 'No user found' })
+
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async getUsers(req, res) {
+        try {
+            const users = await User.find().populate('thoughts')
+            res.json(users)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body)
+            return res.json(user)
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    },
+}
